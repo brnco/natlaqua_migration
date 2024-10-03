665d0a21d67a7efeaaa1ef45ac365b14a9e448c7
Brendan Coates <brendan.emerson.coates@gmail.com>
2024-10-03 10:13:25 -0700

    testing still, trying to get authentication:

--- migrate_photoshelter.py
+++ migrate_photoshelter.py
@@ -2,16 +2,36 @@
 migrates still image data from photoshelter
 '''
 import os
+import json
 import pathlib
 import requests
 import http.client
 
-conn = http.client.HTTPSConnection("www.photoshelter.com")
 
-conn.request("GET", "/psapi/v4.0//library")
+def get_credentials():
+    '''
+    reads api key from config file
+    '''
+    this_dirpath = pathlib.Path(__file__).parent.absolute()
+    with open(this_dirpath / 'api_keys.json', 'r') as config_file:
+        credentials = json.load(config_file)
+    return credentials
 
-response = conn.getresponse()
 
-data = response.read()
+def main():
+    '''
+    do the thing
+    '''
+    cred = get_credentials()
+    params = {"api_key": cred['api_key'],
+              "password": cred['password'],
+              "email": cred['email'],
+              "mode": "token"}
+    headers = {"content-type": "application/x-www-form-urlencoded"}
+    base_url = "https://www.photoshelter.com/psapi/v4.0/authenticate"
+    response = requests.post(base_url, headers=headers, params=params)
+    print(response.__dict__)
 
-print(data.decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
