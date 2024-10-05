c96e5285ea2bc1dd8d6eac42ce26bda880a447d6
Brendan Coates <brendan.emerson.coates@gmail.com>
2024-10-05 10:28:49 -0700

    shaky but something works

--- photoshelter.py
+++ photoshelter.py
@@ -4,6 +4,7 @@ Photoshelter API integration for National Aquarium
 import json
 import requests
 import pathlib
+from pprint import pprint
 
 
 def get_credentials():
@@ -16,7 +17,20 @@ def get_credentials():
     return credentials
 
 
-def main():
+def get_media_md(media_id, token):
+    '''
+    gets metadata for a single media object
+    '''
+    cred = get_credentials()
+    params = {"api_key": cred['api_key']}
+    headers = {"content-type": "application/x-www-form-urlencoded",
+               "X-PS-Api-Key": cred['api_key']}
+    response = requests.get("https://www.photoshelter.com/psapi/v4.0/media/" + media_id + "/metadata", headers=headers)
+    pprint(response.request.__dict__)
+    pprint(response.__dict__)
+
+
+def authenticate():
     '''
     do the thing
     '''
@@ -28,8 +42,18 @@ def main():
     headers = {"content-type": "application/x-www-form-urlencoded"}
     base_url = "https://www.photoshelter.com/psapi/v4.0/authenticate"
     response = requests.post(base_url, headers=headers, params=params)
-    print(response.__dict__)
+    print(response.url)
+    print("click on that ^ url and copy the token it gives you")
 
+
+def main():
+    '''
+    do the thing
+    '''
+    #authenticate()
+    token = "ie6jWgGDDbz5uRkrRXMt"
+    media_id = "I0000xmxpmIut5PM"
+    get_media_md(media_id, token)
 
 if __name__ == "__main__":
     main()
