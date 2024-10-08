14743a670be2256a32a4f9159959c0cd7491490b
Brendan Coates <brendan.emerson.coates@gmail.com>
2024-10-08 16:17:06 -0700

    add a bunch of stuff for airtable integration

--- airtable.py
+++ airtable.py
@@ -20,6 +20,92 @@ def config():
     return atbl_config
 
 
+def get_api_key():
+    '''
+    returns personal token from config file
+    '''
+    atbl_config = config()
+    return atbl_config['airtable']['api_key']
+
+
+class NatlAquaAirtableRecord(object):
+    '''
+    super class for the various AirtableRecord() classes
+    that we'll create later
+    defines a few methods we'll use for all Airtable records
+    '''
+    primary_field = None
+
+    def _get_primary_key_info(self):
+        '''
+        for send()
+        gets primary key name and value
+        '''
+        atbl_tbl = self.get_table()
+        atbl_tbl_schema = atbl_mtd.get_table_schema(atbl_tbl)
+        primary_field_id = atbl_tbl_schema['primaryFieldId']
+        for field in atbl_tbl_schema['fields']:
+            if field['id'] == primary_field_id:
+                primary_field_name = field['name']
+                break
+        try:
+            self_primary_field_value = self._fields[primary_fields_name]
+        except KeyError:
+            self_primary_field_value = self.primary_field
+        return primary_field_name, self_primary_field_value
+
+    def _fill_remote_rec_from_local(self, atbl_rec_remote):
+        '''
+        we can't just assign a record id to an unsaved record
+        and have it overwrite the remote record
+        so instead we do this
+        where we tak ethe remote record
+        and fill it with local values
+        '''
+        for field, value in self._fields.items():
+            try:
+                atbl_rec_remote._fields[field] = value
+            except (KeyError, TypeError) as exc:
+                pprint(exc)
+                continue
+            return atbl_rec_remote
+
+    def _save_rec(self, atbl_rec):
+        '''
+        actually save the record
+        '''
+        try:
+            atbl_rec.save()
+            time.sleep(0.1)
+            if atbl_rec.exists():
+                return atbl_rec
+            else:
+                raise RuntimeError("there was a problem saving that record")
+            except requests.exceptions.HTTPError as exc:
+                pprint(exc)
+                raise RuntimeError("there was a network problem while saving that record")
+
+
+    def send(self):
+        '''
+        primary means of updating and inserting records
+
+        looks up value of primary field in table
+        if found, updates record
+        if not found, creates new record
+        '''
+        primary_field_name, self_primary_field_value = \
+                self._get_primary_key_info()
+        atbl_rec_remote = self._search_on_primary_field(
+                primary_field_name, self_primary_field_value)
+        if atbl_rec_remote:
+            atbl_rec_remote = self._fill_remote_rec_from_local(
+                    atbl_rec_remote)
+        else:
+            atbl_rec_remote = self
+        atbl_rec_remote = self._save_rec(atbl_rec_remote)
+        return atbl_rec_remote
+
 def connect_one_table(base_id, table_name, api_key):
     '''
     connects to Airtable table
