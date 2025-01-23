66dc328af354df3fb1b2f7899f94655949595931
Brendan Coates <brendan.emerson.coates@gmail.com>
2025-01-23 08:19:28 -0800

    updates

--- dedupe.py
+++ dedupe.py
@@ -18,6 +18,7 @@ import os
 import csv
 import pathlib
 import argparse
+import airtable
 
 def process_csv_files(folder_path, output_file):
     '''
@@ -52,6 +53,42 @@ def process_csv_files(folder_path, output_file):
         print(f"Error writing to {output_file}: {e}")
 
 
+def select_files_to_delete():
+    '''
+    goes through Airtable and selects one version of each file to delete
+    '''
+    with open("duplicate_media_ids.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        csv_data = list(csv_reader)
+    media_ids_raw = [x[0] for x in csv_data]
+    media_ids_found = []
+    atbl_conf = airtable.config()
+    atbl_tbl_batch9 = airtable.connect_one_table("app9T4BK04B1G5AAU",
+                                                 "Batch9", atbl_conf['api_key'])
+    atbl_tbl_batch7 = airtable.connect_one_table("appXuHi7ju64VJcVe",
+                                                 "Batch7", atbl_conf['api_key'])
+    atbl_tbl_batch5 = airtable.connect_one_table("appa0ly6gIEsLMztS",
+                                                 "Batch5", atbl_conf['api_key'])
+    atbl_tbl_batch4 = airtable.connect_one_table("appfvA08UNoLP71qC",
+                                                 "Batch4", atbl_conf['api_key'])
+    tables = [atbl_tbl_batch9, atbl_tbl_batch7, atbl_tbl_batch5, atbl_tbl_batch4]
+    for tbl in tables:
+        print(f"working on table {tbl}")
+        for atbl_rec in tbl.all(view="dedupe"):
+            media_id = atbl_rec['fields']['media_id']
+            if media_id not in media_ids_found:
+                tbl.update(atbl_rec['id'], {"delete": True})
+                media_ids_found.append(media_id)
+    '''
+    for atbl_rec in atbl_tbl_batch9.all(view="dedupe"):
+        media_ids_found.append(atbl_rec['fields']['media_id'])
+    for atbl_rec in atbl_tbl_batch7.all(view="dedupe"):
+        media_id = atbl_rec['fields']['media_id']
+        if media_id not in media_ids_found:
+            media_ids_found.append(media_id)
+    for atbl_rec in atbl_tbl_batch
+    '''
+
 def delete_duplicate_files():
     '''
     loops through list of duplicate files
@@ -70,7 +107,7 @@ def delete_duplicate_files():
     create those views
     then write all this
     '''
-
+    
 
 
 
@@ -82,6 +119,7 @@ def init_args():
             formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("--mode", dest="mode",
                         choices=['find_duplicates',
+                                 'select_files_to_delete',
                                  'delete_duplicate_files'])
     args = parser.parse_args()
     return args
@@ -96,6 +134,8 @@ def main():
         folder_path = pathlib.Path("/home/bcoates/code/natlaqua_migration/dedupe/")
         output_file = "duplicate_media_ids.csv"
         process_csv_files(folder_path, output_file)
+    elif args.mode == 'select_files_to_delete':
+        select_files_to_delete()
     elif args.mode == 'delete_duplicate_files':
         delete_duplicate_files()
 
